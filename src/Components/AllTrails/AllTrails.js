0b3f16c3237d137af60c1684b0a2b4e5e9a0d220
Cody Shaben <cshaben87@gmail.com>
2020-03-01 14:04:46 -0700

    set up map view container

--- src/Components/AllTrails/AllTrails.js
+++ src/Components/AllTrails/AllTrails.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import api from '../../api.js';
 import RingLoader from 'react-spinners/RingLoader';
+import MapView from '../MapView/MapView'
 import './AllTrails.scss';
 
 const AllTrails = (props) => {
-    const { limit, onLoadMore, trailContainer } = props;
+    const { limit, onLoadMore, trailContainer, mapContainer } = props;
 
     const [trails, setTrails] = useState([]);
     const [loadMoreButton, setLoadMoreButton] = useState(false);
     const [loading, setIsLoading] = useState(true);
+    const [listView, setListView] = useState(true);
+    const [mapView, setMapView] = useState(false)
+
+    const showListView = () => {
+        setListView(true) 
+        setMapView(false)
+        setLoadMoreButton(true)
+    }
+
+    const showMapView = () => {
+        setListView(false) 
+        setMapView(true)
+        setLoadMoreButton(false)
+    }
 
     useEffect(() => {
         async function fetchData() {
@@ -28,24 +43,31 @@ const AllTrails = (props) => {
     const showLoading = () => {
         return (
             <div className='loading-spinner'>
-                <RingLoader
-                    // css={override}
-                    size={150}
-                    color={'rgb(11, 125, 201)'}
-                    loading={loading}
-                />
+                <RingLoader size={150} color={'rgb(11, 125, 201)'} loading={loading}/>
                 <h3>Loading...</h3>
             </div>
         );
     };
+
+
     
     return (
         <div className="all-trails">
-            {trails.slice(0, limit).map(trail => {
-                return trailContainer(trail)
-            })}
             { loading === true ? showLoading() : null }
+
+            <section className='views'>
+                    <button className='list-view' onClick={showListView}>List</button>
+                    <p>|</p>
+                    <button className='map-view' onClick={showMapView}>Map</button>
+            </section> 
+            {/* {trails.slice(0, limit).map(trail => {
+                return trailContainer(trail)
+            })} */}
+            { listView === true ? trails.slice(0, limit).map(trail => {
+                return trailContainer(trail)
+            }): mapContainer() }
             { loadMoreButton === true ? <button onClick={onLoadMore} className='load-more'>Load More</button> : null }
+            
         </div>
     );
 };
--- src/Components/Places/Places.js
+++ src/Components/Places/Places.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import AllTrails from '../AllTrails/AllTrails';
-import RatingDropdown from '../RatingDropdown/RatingDropdown'
-import DifficultyDropdown from '../DifficultyDropdown/DifficultyDropdown'
+import RatingDropdown from '../RatingDropdown/RatingDropdown';
+import DifficultyDropdown from '../DifficultyDropdown/DifficultyDropdown';
 import FiveStarTrails from '../AllTrails/FiveStarTrails';
 import FourStarTrails from '../AllTrails/FourStarTrails';
 import ThreeStarTrails from '../AllTrails/ThreeStarTrails';
 import TwoStarTrails from '../AllTrails/TwoStarTrails';
 import OneStarTrails from '../AllTrails/OneStarTrails';
 import StarRatings from 'react-star-ratings';
+import noImage from './Images/image-not-found.png'
+// import black from './Images/Black.png'
+// import dblack from './Images/DoubleBlack.png'
+// import blueBlack from './Images/BlueBlack.png'
+// import blue from './Images/Blue.png'
+// import greenBlue from './Images/GreenBlue.png'
+// import green from './Images/Green.png'
 import './Places.scss';
 import { useRoutes } from 'hookrouter';
 
@@ -16,7 +23,7 @@ function Places() {
     const [isRatingOpen, setRatingOpen ] = useState(false);
     const [isDifficultyOpen, setDifficultyOpen ] = useState(false);
 
-    const hideRatingDropDown = () => setRatingOpen(false)
+    const hideRatingDropDown = () => setRatingOpen(false);
     const toggleRatingDropDown = () => setRatingOpen(!isRatingOpen);
     const toggleDifficultyDropDown = () => setDifficultyOpen(!isDifficultyOpen);
     const onLoadMore = () => setLimit(limit + 10);
@@ -28,6 +35,11 @@ function Places() {
         : difficulty === 'blue' ? 'Blue'
         : difficulty === 'greenBlue' ? 'Green Blue'
         : 'Green'
+    };
+
+    const imageReplacement = (error) => {
+        error.target.src=noImage
+        return true
     }
 
     const trailContainer = (trail) => {
@@ -37,45 +49,47 @@ function Places() {
                     <h4>{trail.name}</h4>
                     <p className='location'>{trail.location}</p>
                     <p className='star-text'>{trail.stars} Stars  | {trail.starVotes} Reviews</p>
-                    <StarRatings
-                        rating={trail.stars}
-                        starRatedColor='rgb(11, 125, 201)'
-                        starDimension='30px'
-                    />
+                    <StarRatings rating={trail.stars} starRatedColor='rgb(11, 125, 201)' starDimension='30px'/>
                     <p className='summary'>{trail.summary}</p>
                 </div> 
                 <div className='left-side trail-info'>
-                    <p className='trail-details'>Difficulty | {trailDifficultySymbols(trail.difficulty)}</p>
+                    <p className='trail-details symbol'>Difficulty | {trailDifficultySymbols(trail.difficulty)}</p>
                     <p className='trail-details'>Length | {trail.length} Miles</p>
                     <p className='trail-details'>Ascent | {trail.ascent} Feet</p>
                     <p className='trail-details'>Descent | {trail.descent} Feet</p>
                     <p className='trail-details'>Highest Altitude | {trail.high} Feet</p>
                     <p className='trail-details'>Lowest Altitude | {trail.low} Feet</p>
-
                 </div>
-                <img className='image' src={trail.imgMedium} alt='Not Found'></img>
+                <img 
+                    className='image' 
+                    src={trail.imgMedium} 
+                    alt='Trail Image'
+                    onError={imageReplacement}
+                    >
+                </img>
             </section>
+        );
+    };
+
+    const mapContainer = (trail) => {
+        return (
+                <p className='map-view'>hello from the map container</p>
         )
     }
 
     const routes = {
-        '/all-trails': () => <AllTrails trailContainer={trailContainer} limit={limit} onLoadMore={onLoadMore}/>,
-        '/five-star': () => <FiveStarTrails trailContainer={trailContainer} limit={limit} onLoadMore={onLoadMore}/>,
-        '/four-star': () => <FourStarTrails trailContainer={trailContainer} limit={limit} onLoadMore={onLoadMore}/>,
-        '/three-star': () => <ThreeStarTrails trailContainer={trailContainer} limit={limit} onLoadMore={onLoadMore}/>,
-        '/two-star': () => <TwoStarTrails trailContainer={trailContainer} limit={limit} onLoadMore={onLoadMore}/>,
-        '/one-star': () => <OneStarTrails trailContainer={trailContainer} limit={limit} onLoadMore={onLoadMore}/>,
+        '/all-trails': () => <AllTrails mapContainer={mapContainer} trailContainer={trailContainer} limit={limit} onLoadMore={onLoadMore}/>,
+        '/five-star': () => <FiveStarTrails mapContainer={mapContainer} trailContainer={trailContainer} limit={limit} onLoadMore={onLoadMore}/>,
+        '/four-star': () => <FourStarTrails mapContainer={mapContainer} trailContainer={trailContainer} limit={limit} onLoadMore={onLoadMore}/>,
+        '/three-star': () => <ThreeStarTrails mapContainer={mapContainer} trailContainer={trailContainer} limit={limit} onLoadMore={onLoadMore}/>,
+        '/two-star': () => <TwoStarTrails mapContainer={mapContainer} trailContainer={trailContainer} limit={limit} onLoadMore={onLoadMore}/>,
+        '/one-star': () => <OneStarTrails mapContainer={mapContainer} trailContainer={trailContainer} limit={limit} onLoadMore={onLoadMore}/>,
     };
 
     const routeResult = useRoutes(routes);
 
     return (
         <div className='places' >   
-            <section className='views'>
-                    <button className='list-view'>List</button>
-                    <p>|</p>
-                    <button className='map-view'>Map</button>
-            </section> 
             <section className='dropdown-menu'>
                 <header>Sort by</header>
                 <div className='dropdown-title'>
