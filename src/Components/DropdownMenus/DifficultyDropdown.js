0d154e99b5b55b471025ac58fff95620eadc1d15
Cody Shaben <cshaben87@gmail.com>
2020-05-03 09:07:40 -0600

    cleaned up dropdown components css

---
+++ src/Components/DropdownMenus/DifficultyDropdown.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './DropDown.scss';
+
+const DifficultyDropdown = (props) => {
+    const { setFetchUrl } = props;
+
+    const filterDblack = () => setFetchUrl('double-black');
+    const filterBlack = () => setFetchUrl('black');
+    const filterBlue = () => setFetchUrl('blue');
+    const filterGreen = () => setFetchUrl('green');
+
+    return (
+        <ul className='dropdown-list' id='difficulty-list'>
+            <button className='dropdown-li' onClick={filterDblack}>Double Black</button>
+            <button className='dropdown-li' onClick={filterBlack}>Black</button>
+            <button className='dropdown-li' onClick={filterBlue}>Blue</button>
+            <button className='dropdown-li' onClick={filterGreen}>Green</button>
+        </ul>
+    );
+};
+
+export default DifficultyDropdown;
---
+++ src/Components/DropdownMenus/RatingDropdown.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './DropDown.scss'
+
+const RatingDropdown = (props) => {
+    const { setFetchUrl } = props
+
+    const filterFiveStars = () => setFetchUrl('five-star')
+    const filterFourStars = () => setFetchUrl('four-star')
+    const filterThreeStars = () => setFetchUrl('three-star')
+    const filterTwoStars = () => setFetchUrl('two-star')
+    const filterOneStars = () => setFetchUrl('one-star')
+
+
+    return(
+        <ul className='dropdown-list' id='rating-list'>
+            <button  className='dropdown-li' onClick={filterFiveStars}>Five Star</button> 
+            <button className='dropdown-li' onClick={filterFourStars}>Four Star</button>
+            <button className='dropdown-li' onClick={filterThreeStars}>Three Star</button>
+            <button className='dropdown-li' onClick={filterTwoStars}>Two Star</button>
+            <button className='dropdown-li' onClick={filterOneStars}>One Star</button>
+        </ul>
+    )
+}
+
+export default RatingDropdown
