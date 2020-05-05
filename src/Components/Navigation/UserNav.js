258a0a9fb2602c0872fb30c4b1fdeb5add3858c5
Cody Shaben <cshaben87@gmail.com>
2020-05-05 14:16:37 -0600

    tweaked some dashboard css

--- src/Components/Navigation/UserNav.js
+++ src/Components/Navigation/UserNav.js
@@ -11,28 +11,13 @@ const UserNav = (props) => {
         window.location = '/login'
       };
 
-    const handleDashboardClick = (e) => {
-      e.stopPropagation()
-      window.location = `/home/${id}`
-    }
-
-    const handleMessageBoardClick = (e) => {
-      e.stopPropagation()
-      window.location = `/home/${id}/message-board`
-    }
-
-    const handleAllTrailsClick = (e) => {
-      e.stopPropagation()
-      window.location = `/home/${id}/all-trails`
-    }
-
     return (
       <div className='nav' >
-        <a onClick={handleDashboardClick} className='loggedIn-nav'>Dashboard</a>
-        <a onClick={handleMessageBoardClick} className='loggedIn-nav'>Message Board</a>
-        <a onClick={handleAllTrailsClick} className='loggedIn-nav'>All Trails</a>
+        <a href={`/home/${id}`}className='loggedIn-nav'>Dashboard</a>
+        <a href={`/home/${id}/message-board`} className='loggedIn-nav'>Message Board</a>
+        <a href={`/home/${id}/all-trails`} className='loggedIn-nav'>All Trails</a>
         <a 
-          to='/login'
+          href='/login'
           className='loggedIn-nav' 
           onClick={logOut}
         >Log Out</a>
